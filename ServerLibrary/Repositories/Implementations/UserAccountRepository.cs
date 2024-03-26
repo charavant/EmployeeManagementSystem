1f4ab649bbd0083785417af10df8883d95128292
charavant <65118389+charavant@users.noreply.github.com>
2024-03-26 16:53:13 +0200

    .

---
+++ ServerLibrary/Repositories/Implementations/UserAccountRepository.cs
@@ -0,0 +1,24 @@
+﻿using BaseLibrary.DTOs;
+using BaseLibrary.Responses;
+using ServerLibrary.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerLibrary.Repositories.Implementations
+{
+  public class UserAccountRepository : IUserAccount
+  {
+    public Task<GeneralResponce> CreateAsync(Register user)
+    {
+      throw new NotImplementedException();
+    }
+
+    public Task<LoginResponce> SignInAsync(Login user)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
